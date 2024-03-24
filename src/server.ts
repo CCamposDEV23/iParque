b25e01f736a32c667fb9d8941a62f30f71677a7c
Carolina Campos <carolinacampos.dev23@gmail.com>
2024-03-24 20:37:56 -0300

    logincadastro

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import con from './connection';
+
 
 const server = express();
 
 server.get("/", () => {
   console.log("BATEU");
+});
+
+server.get("/usuario", () => {
+  con.query('SELECT * FROM USUARIO ORDER BY nome DESC', (err, result) => {
+    if(err){
+      res.send(err)
+    }
+    res.send(result);
+  })
 });
 export default server;
